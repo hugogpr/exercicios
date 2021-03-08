c2ef067d17cb0e955000f0728a7b461e0bcc901a
Hugo Gabriel <hugo.gabriel@wdna.com.br>
2021-03-08 13:40:48 -0300

    08-03 13:40

---
+++ sessao_17/evento_load/js/script.js
@@ -0,0 +1,9 @@
+/* 
+window.addEventListener('load', function(){
+    alert("Assine os nossos termos de uso");
+}); 
+*/
+
+window.addEventListener('beforeunload', function(e){
+    event.returnValue = null;
+});
