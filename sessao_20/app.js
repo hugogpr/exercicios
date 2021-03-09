366df7fd733d0ce1342e013ec5a342e1bc8cc7bd
Hugo Gabriel <hugo.gabriel@wdna.com.br>
2021-03-09 16:19:33 -0300

    09-03 16:19

--- sessao_20/app.js
+++ sessao_20/app.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
+const express       = require('express');
+const app           = express();
+const db            = require('./db/connection');
+const bodyParser    = require('body-parser');
 
 const PORT = 3000;
 
@@ -7,6 +9,23 @@ app.listen(PORT,function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 });
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+//db connection
+db.authenticate()
+.then(()=>{
+    console.log("conectou ao banco com sucesso");
+}).catch(err =>{
+    console.log("ocorreu um erro ao conectar",err);
+});
+
+
+//routes
 app.get('/',(req,res)=>{
     res.send("Esta funcionando 3");
 });
+
+//Jobs routes
+app.use('/jobs', require('./routes/jobs'));
