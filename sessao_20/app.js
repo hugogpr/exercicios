1d436bca15a518fbfc414bb21a7e34bfec52692a
Hugo Gabriel <hugo.gabriel@wdna.com.br>
2021-03-10 15:18:37 -0300

    concluido

--- sessao_20/app.js
+++ sessao_20/app.js
@@ -1,30 +1,70 @@
 const express       = require('express');
+const exphbs        = require('express-handlebars');
 const app           = express();
+const path          = require('path');
 const db            = require('./db/connection');
 const bodyParser    = require('body-parser');
+const Job           = require('./models/Job');
+const Sequelize		= require('sequelize');
+const Op			= Sequelize.Op;
 
 const PORT = 3000;
 
 app.listen(PORT,function(){
-    console.log(`O Express está rodando na porta ${PORT}`);
+	console.log(`O Express está rodando na porta ${PORT}`);
 });
 
 //body parser
 app.use(bodyParser.urlencoded({extended: false}));
 
+//handle bars
+app.set('views', path.join(__dirname,'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use(express.static(path.join(__dirname,'public')));
 
 //db connection
 db.authenticate()
 .then(()=>{
-    console.log("conectou ao banco com sucesso");
+	console.log("conectou ao banco com sucesso");
 }).catch(err =>{
-    console.log("ocorreu um erro ao conectar",err);
+	console.log("ocorreu um erro ao conectar",err);
 });
 
 
 //routes
 app.get('/',(req,res)=>{
-    res.send("Esta funcionando 3");
+
+	let search = req.query.job;
+	let query = '%'+search+'%';
+
+	if(!search){
+
+		Job.findAll({order: [
+			['createdAt','DESC']
+		]})
+		.then(jobs => {
+
+			res.render('index',{
+				jobs
+			});
+		}).catch(err => console.log(err));
+	} else {
+
+		Job.findAll({
+			where: {title: {[Op.like]: query}},
+			order: [
+				['createdAt','DESC']
+		]})
+		.then(jobs => {
+
+			res.render('index',{
+				jobs , search
+			});
+		}).catch(err => console.log(err));
+	}
 });
 
 //Jobs routes
